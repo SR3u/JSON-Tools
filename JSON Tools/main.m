b5897f80902823244eef72c88b8733e6a504142b
Sergey Rump <wnmsr3u@gmail.com>
2015-04-14 15:10:40 +0300

    Initial Commit

---
+++ JSON Tools/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  JSON Tools
+//
+//  Created by Sergey Rump on 14.04.15.
+//  Copyright (c) 2015 SR3u. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
