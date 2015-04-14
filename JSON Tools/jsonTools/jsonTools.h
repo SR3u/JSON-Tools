402b8f904e7773afadbafa2ffb37809ed845f79f
Sergey Rump <wnmsr3u@gmail.com>
2015-04-14 16:10:27 +0300

    some comments

--- JSON Tools/jsonTools/jsonTools.h
+++ JSON Tools/jsonTools/jsonTools.h
@@ -9,14 +9,14 @@
 #import <Foundation/Foundation.h>
 
 @interface NSDictionary (jsonTools)
-+(NSString*) jsonToolsVersion;
 -(NSString*) jsonString;
--(NSString*) jsonStringWithPrettyPrint:(BOOL) prettyPrint;
+-(NSString*) jsonStringWithPrettyPrint:(BOOL) prettyPrint;//prettyPrint means more human-readable
 +(NSDictionary*) dictionaryWithJSONString:(NSString*) jsonString;
++(NSString*) jsonToolsVersion;//returns string with current jsonTools version for this class
 @end
 @interface NSArray (jsonTools)
-+(NSString*) jsonToolsVersion;
 -(NSString*) jsonString;
-- (NSString *)jsonStringWithPrettyPrint:(BOOL)prettyPrint;
+- (NSString *)jsonStringWithPrettyPrint:(BOOL)prettyPrint;//prettyPrint means more human-readable
 +(NSArray*) arrayWithJSONString:(NSString*) jsonString;
++(NSString*) jsonToolsVersion;//returns string with current jsonTools version for this class
 @end
