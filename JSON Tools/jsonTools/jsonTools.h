bd8001d2cf664e9f4407cb251d352e1b1aa589a0
Sergey Rump <wnmsr3u@gmail.com>
2015-04-14 15:35:46 +0300

    minor fixes

--- JSON Tools/jsonTools/jsonTools.h
+++ JSON Tools/jsonTools/jsonTools.h
@@ -1,6 +1,6 @@
 //
-//  Tools.h
-//  Dress
+//  jsonTools.h
+//  JSON Tools
 //
 //  Created by Sergey Rump on 14.04.15.
 //  Copyright (c) 2015 SR3u. All rights reserved.
--- JSON Tools/jsonTools/jsonTools.m
+++ JSON Tools/jsonTools/jsonTools.m
@@ -1,6 +1,6 @@
 //
-//  Tools.m
-//  Dress
+//  jsonTools.m
+//  JSON Tools
 //
 //  Created by Sergey Rump on 14.04.15.
 //  Copyright (c) 2015 SR3u. All rights reserved.
